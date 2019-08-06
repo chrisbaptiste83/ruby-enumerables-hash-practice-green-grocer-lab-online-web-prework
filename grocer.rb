be8260f99315af996f0fc71a9b2baf082ac6114a
Christopher Baptiste <chrisbaptiste83@gmail.com>
2019-08-06 13:25:50 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,19 +1,5 @@
 def consolidate_cart(item)
 
-  count = :count
-item.each do |hash|
-  hash.each do |food, description|
-  
-if final.has_key?(food) == false
-  final[food] = description
-  final[food][count] = 1
-elsif final.has_key?(food)
-final[food][:count] +=1
-end
-final
-end
-end 
-
 
 def apply_coupons(cart, coupons)
   # code here
