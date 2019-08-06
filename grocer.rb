a122a761df5b4c83323b344cf9a21f6b5b8f2549
Christopher Baptiste <chrisbaptiste83@gmail.com>
2019-08-06 13:33:57 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,7 +1,7 @@
 def consolidate_cart(cart)
   new_cart = {}
   cart.each do |items_array|
-    items_array.each do |item, attributr_hash|
+    items_array.each do |item, attribute_hash|
       new_cart[item] ||=attribute_hash 
       new_cart[item][:count] ? new_cart[item][:count] +=1:
       new_cart[item][:count] = 1 
